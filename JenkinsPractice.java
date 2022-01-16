5665149871a63ee39e396a09e27eb6d86cce283b
jcharlesarun <jcharlesarun@gmail.com>
2022-01-16 16:24:47 +0530

    Changes done

--- JenkinsPractice.java
+++ JenkinsPractice.java
@@ -1,8 +1,8 @@
 class JenkinsPractice {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Arun!");
+        System.out.println("Hello, Charles!");
+        System.out.println("Hello, Karthika!");
     }
 }
