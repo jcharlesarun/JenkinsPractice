ec250ae9c634e5a34c345ab5399843ded60ed233
jcharlesarun <42461281+jcharlesarun@users.noreply.github.com>
2022-01-16 16:28:05 +0530

    Update JenkinsPractice.java
    Child Branch changes

--- JenkinsPractice.java
+++ JenkinsPractice.java
@@ -4,5 +4,9 @@ class JenkinsPractice {
         System.out.println("Hello, Arun!");
         System.out.println("Hello, Charles!");
         System.out.println("Hello, Karthika!");
+        System.out.println("Hello, World!"); 
+        System.out.println("Hello, Arun!");
+        System.out.println("Hello, Charles!");
+        System.out.println("Hello, Karthika!");
     }
 }
